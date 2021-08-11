ead809ca2945462fa1fb85445b1e389d9f3f59b1
Claudemir Ramos Ferreira <c.ramos.ferreira@avanade.com>
2021-08-11 16:56:23 -0300

    commit initial

---
+++ src/test/java/br/com/setebit/deliveryservice/DeliveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.setebit.deliveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
